c11c1368b467233ea21c8c69dad121840615efe6
ratinoray <ratinoray@gmail.com>
2024-09-19 15:53:00 +0000

    tasks controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,20 +1,29 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CreateTaskDto } from './dtos/create-task.dto';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
 
+    tasksService: TasksService;
+
+    constructor(){
+        this.tasksService = new TasksService();
+    }
+
     @Get()
-    listTask(){}
+    listTask(){
+        return this.tasksService.findAll();
+    }
 
     @Post()
     createTask(@Body() body: CreateTaskDto){
-        return body;
+        return this.tasksService.create(body.content);
     }
 
     @Get('/:id')
     getTask(@Param('id') id: string){
-        return {id: id};
+        return this.tasksService.findOne(parseInt(id));
     }
 
     
